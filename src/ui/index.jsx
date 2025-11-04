c9f4f5266a7da57dd346bfd634a18c3c13bc504e
Dehayez <yentel.dh@hotmail.be>
2025-11-04 01:47:02 +0100

    Game Menu Tab

--- src/ui/index.jsx
+++ src/ui/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App.jsx';
-import '../styles.scss';
+import '../styles.css';
 
 // Initialize React app
 const container = document.getElementById('ui-root');
